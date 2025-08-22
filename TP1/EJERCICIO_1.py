6e5a8eff705de1caba981b3487af320aa746f334
mhoffer1 <matiashoffer@hotmail.com>
2025-08-22 13:31:57 -0300

    add initial implementations for exercises 1 to 9 in TP1

---
+++ TP1/EJERCICIO_1.py
@@ -0,0 +1,8 @@
+"""
+Desarrollar una función que reciba tres números enteros positivos y devuelva el 
+mayor de los tres, sólo si éste es único (es decir el mayor estricto). Devolver -1 en 
+caso de no haber ninguno. No utilizar operadores lógicos (and, or, not). Desarrollar 
+también un programa para ingresar los tres valores, invocar a la función y mostrar 
+el máximo hallado, o un mensaje informativo si éste no existe.
+"""
+

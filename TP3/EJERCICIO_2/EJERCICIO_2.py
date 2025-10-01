f1e1dee8dc853c97e2fbb08960202f8f289ce0d3
Matias Hoffer <matiashoffer@hotmail.com>
2025-10-01 13:31:17 -0300

    correcion de ejerccios

--- TP3/EJERCICIO_2/EJERCICIO_2.py
+++ TP3/EJERCICIO_2/EJERCICIO_2.py
@@ -1,3 +1,228 @@
 """
+Las siguientes matrices responden distintos patrones de relleno. 
+Desarrollar funciones que generen cada una de ellas sin intervención humana y escribir un programa
+que las invoque e imprima por pantalla. 
+El tamaño de las matrices debe establecerse como N x N, donde N se ingresa a través del teclado.
+
+a: 01 00 00 00  b: 00 00 00 27   c: 04 00 00 00 
+   00 03 00 00     00 00 09 00      03 03 00 00 
+   00 00 05 00     00 03 00 00      02 02 02 00 
+   00 00 00 07     01 00 00 00      01 01 01 01
+
+d: 08 08 08 08  e: 00 01 00 02   f: 00 00 00 01 
+   04 04 04 04     03 00 04 00      00 00 03 02 
+   02 02 02 02     00 05 00 06      00 06 05 04 
+   01 01 01 01     07 00 08 00      10 09 08 07
+
+g: 01 02 03 04  h: 01 02 04 07   i: 01 02 06 07
+   12 13 14 05     03 05 08 11      03 05 08 13
+   11 16 15 06     06 09 12 14      04 09 12 14
+   10 09 08 07     10 13 15 16      10 11 15 16
 
 """
+import tabulate
+
+def crear_matriz(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con ceros.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con ceros.
+    """
+    return [[0] * n for _ in range(n)]
+
+def imprimir_matriz(matriz: list[list[int]], matriz_nombre: str) -> None:
+    """
+    Imprime la matriz en un formato legible.
+    args:
+        matriz (list[list[int]]): Matriz a imprimir.
+        matriz_nombre (str): Nombre de la matriz a imprimir.
+    """
+    print(f"Matriz {matriz_nombre}:")
+    print(tabulate.tabulate(matriz, tablefmt="plain"))
+    print()
+
+def matriz_a(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz a.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz a.
+    """
+    matriz = crear_matriz(n)
+    for i in range(n):
+        matriz[i][i] = 2 * i + 1
+    return matriz
+
+def matriz_b(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz b.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz b.
+    """
+    matriz = crear_matriz(n)
+    for i in range(n):
+        matriz[i][n - 1 - i] = 3 ** i
+    return matriz
+
+def matriz_c(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz c.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz c.
+    """
+    matriz = crear_matriz(n)    
+    for i in range(n):
+        for j in range(i + 1):
+            matriz[i][j] = n - i
+    return matriz
+
+def matriz_d(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz d.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz d.
+    """
+    matriz = crear_matriz(n)
+    
+    val = n ** 2
+    for i in range(n):
+        for j in range(n):
+            matriz[i][j] = int(val / 2)
+        val = val / 2
+    return matriz
+
+def matriz_e(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz e.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz e.
+    """
+    matriz = crear_matriz(n)
+    contador = 1
+
+    for i in range(n):
+        for j in range(n):
+            if (i + j) % 2 == 1:
+                matriz[i][j] = contador
+                contador += 1
+    return matriz
+
+def matriz_f(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz f.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz f.
+    """
+    matriz = crear_matriz(n)
+    for i in range( 1, n + 1 ):             
+        triangular_i = i * ( i + 1 ) // 2  #triangular de i 
+        for j in range( 1, n + 1 ): 
+            if i + j > n:
+                matriz[ i - 1 ][ j - 1 ] = triangular_i - ( i + j - ( n + 1 )) #triangular de i - (i + j - (n + 1))
+    return matriz
+
+
+def matriz_g(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz g.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz g.
+    """
+    matriz = crear_matriz(n)
+    
+    superior = 0
+    inferior = n - 1
+    izquierda = 0
+    derecha = n - 1
+    
+    num = 1
+    
+    while superior <= inferior and izquierda <= derecha:
+
+        # Rellenar la fila superior
+        for col in range(izquierda, derecha + 1):
+            matriz[superior][col] = num
+            num += 1
+        superior += 1
+        
+        # Rellenar la columna derecha
+        for fila in range(superior, inferior + 1):
+            matriz[fila][derecha] = num
+            num += 1
+        derecha -= 1
+        
+        # Rellenar la fila inferior
+        if superior <= inferior:
+            for col in range(derecha, izquierda - 1, -1):
+                matriz[inferior][col] = num
+                num += 1
+            inferior -= 1
+        
+        # Rellenar la columna izquierda
+        if izquierda <= derecha:
+            for fila in range(inferior, superior - 1, -1):
+                matriz[fila][izquierda] = num
+                num += 1
+            izquierda += 1
+    
+    return matriz
+
+def matriz_h(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz h.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz h.
+    """
+    matriz = crear_matriz(n)
+    num = 1
+    for k in range(n * 2 - 1):
+        for i in range(k + 1):
+            j = k - i
+            if i < n and j < n:
+                matriz[i][j] = num
+                num += 1
+    return matriz
+
+def matriz_i(n: int) -> list[list[int]]:
+    """
+    Crea una matriz de tamaño n x n con los valores de la matriz i.
+    args:
+        n (int): Tamaño de la matriz (n x n).
+    returns:
+        list: Matriz de tamaño n x n con los valores de la matriz i.
+    """
+    pass
+    
+
+def main():
+    n = int(input("Ingrese el tamaño de la matriz (N x N): "))
+
+    imprimir_matriz(matriz_a(n), 'A')
+    imprimir_matriz(matriz_b(n), 'B')
+    imprimir_matriz(matriz_c(n), 'C')
+    imprimir_matriz(matriz_d(n), 'D')
+    imprimir_matriz(matriz_e(n), 'E')
+    imprimir_matriz(matriz_f(n), 'F')
+    imprimir_matriz(matriz_g(n), 'G')
+    imprimir_matriz(matriz_h(n), 'H')
+    # imprimir_matriz(matriz_i(n), 'I')
+
+if __name__ == "__main__":
+    main()
--- TP3/EJERCICIO_4/EJERCICIO_4.py
+++ TP3/EJERCICIO_4/EJERCICIO_4.py
@@ -1,228 +1,141 @@
 """
-Las siguientes matrices responden distintos patrones de relleno. 
-Desarrollar funciones que generen cada una de ellas sin intervención humana y escribir un programa
-que las invoque e imprima por pantalla. 
-El tamaño de las matrices debe establecerse como N x N, donde N se ingresa a través del teclado.
+Una fabrica de bicicletas guarda en una matriz la cantidad de unidades producidas 
+en cada una de sus plantas durante una semana. De este modo, cada columna re
+presenta el dia de la semana y cada fila a una de sus fabricas. Ejemplo:
 
-a: 01 00 00 00  b: 00 00 00 27   c: 04 00 00 00 
-   00 03 00 00     00 00 09 00      03 03 00 00 
-   00 00 05 00     00 03 00 00      02 02 02 00 
-   00 00 00 07     01 00 00 00      01 01 01 01
++----------------+--------+---------+-----------+---------+--------+----------+
+|                | Lunes  | Martes  | Miércoles | Jueves  | Viernes| Sabado   |
+|                |   0    |    1    |     2     |    3    |   4    |    5     |
++----------------+--------+---------+-----------+---------+--------+----------+
+| (Fabrica 1) 0  |   23   |   150   |    20     |   120   |   25   |   150    |
+| (Fabrica 2) 1  |   40   |    75   |    80     |    0    |   80   |    35    |
+| (... )         |  ...   |   ...   |   ...     |   ...   |  ...   |   ...    |
+| (Fabrica n) 3  |   80   |    80   |    80     |    80   |   80   |    80    |
++----------------+--------+---------+-----------+---------+--------+----------+
 
-d: 08 08 08 08  e: 00 01 00 02   f: 00 00 00 01 
-   04 04 04 04     03 00 04 00      00 00 03 02 
-   02 02 02 02     00 05 00 06      00 06 05 04 
-   01 01 01 01     07 00 08 00      10 09 08 07
-
-g: 01 02 03 04  h: 01 02 04 07   i: 01 02 06 07
-   12 13 14 05     03 05 08 11      03 05 08 13
-   11 16 15 06     06 09 12 14      04 09 12 14
-   10 09 08 07     10 13 15 16      10 11 15 16
-
+a. Crear una matriz con datos generados al azar para N fabricas durante una 
+   semana, considerando que la capacidad maxima de fabricacion es de 150 
+   unidades por dia y puede suceder que en ciertos dias no se fabrique ninguna. 
+    
+b. Mostrar la cantidad total de bicicletas fabricadas por cada fabrica. 
+    
+c. Cual es la fabrica que mas produjo en un solo dia (detallar dia y fabrica).
+    
+d. Cual es el dia mas productivo, considerando todas las fabricas combinadas.
+    
+e. Crear una lista por comprension que contenga la menor cantidad fabricada 
+   por cada fabrica.
 """
+
+import random
 import tabulate
 
-def crear_matriz(n: int) -> list[list[int]]:
+def crear_matriz_azar(fabricas: int, dias: int, max_produccion: int) -> list[list[int]]:
     """
-    Crea una matriz de tamaño n x n con ceros.
+    Crea una matriz de tamaño fabricas x dias con datos generados al azar.
     args:
-        n (int): Tamaño de la matriz (n x n).
+        fabricas (int): Numero de fabricas (filas).
+        dias (int): Numero de dias (columnas).
+        max_produccion (int): Capacidad maxima de fabricacion por dia.
     returns:
-        list: Matriz de tamaño n x n con ceros.
+        list: Matriz de tamaño fabricas x dias con datos generados al azar.
     """
-    return [[0] * n for _ in range(n)]
+    return [[random.randint(0, max_produccion) for _ in range(dias)] for _ in range(fabricas)]
 
-def imprimir_matriz(matriz: list[list[int]], matriz_nombre: str) -> None:
+def total_por_fabrica(matriz: list[list[int]]) -> list[int]:
     """
-    Imprime la matriz en un formato legible.
+    Calcula la cantidad total de bicicletas fabricadas por cada fabrica.
     args:
-        matriz (list[list[int]]): Matriz a imprimir.
-        matriz_nombre (str): Nombre de la matriz a imprimir.
+        matriz (list[list[int]]): Matriz de produccion.
+    returns:
+        list: Lista con la cantidad total de bicicletas por fabrica.
     """
-    print(f"Matriz {matriz_nombre}:")
-    print(tabulate.tabulate(matriz, tablefmt="plain"))
+    return [sum(fabrica) for fabrica in matriz]
+
+def fabrica_mas_produccion_dia(matriz: list[list[int]]) -> tuple[int, int, int]:
+    """
+    Encuentra la fabrica que mas produjo en un solo dia.
+    args:
+        matriz (list[list[int]]): Matriz de produccion.
+    returns:
+        tuple: (fabrica, dia, cantidad) que mas produjo en un solo dia.
+    """
+    max_produccion = -1
+    fabrica_max = -1
+    dia_max = -1
+    for i, fabrica in enumerate(matriz):
+        for j, produccion in enumerate(fabrica):
+            if produccion > max_produccion:
+                max_produccion = produccion
+                fabrica_max = i
+                dia_max = j
+    return (fabrica_max, dia_max, max_produccion)
+
+def dia_mas_productivo(matriz: list[list[int]]) -> tuple[int, int]:
+    """
+    Encuentra el dia mas productivo considerando todas las fabricas combinadas.
+    args:
+        matriz (list[list[int]]): Matriz de produccion.
+    returns:
+        tuple: (dia, cantidad total) que fue el mas productivo.
+    """
+    dias = len(matriz[0])
+    max_produccion_dia = -1
+    dia_max = -1
+    for j in range(dias):
+        produccion_dia = sum(matriz[i][j] for i in range(len(matriz)))
+        if produccion_dia > max_produccion_dia:
+            max_produccion_dia = produccion_dia
+            dia_max = j
+    return (dia_max, max_produccion_dia)
+
+def menor_cantidad_por_fabrica(matriz: list[list[int]]) -> list[int]:
+    """
+    Crea una lista con la menor cantidad fabricada por cada fabrica.
+    args:
+        matriz (list[list[int]]): Matriz de produccion.
+    returns:
+        list: Lista con la menor cantidad fabricada por cada fabrica.
+    """
+    return [min(fabrica) for fabrica in matriz]
+
+def imprimir_datos(matriz: list[list[int]], nombres_fabricas: list[str], dias_semana: list[str]) -> None:
+    """
+    Imprime los datos de produccion de las fabricas.
+    args:
+        matriz (list[list[int]]): Matriz de produccion.
+        nombres_fabricas (list[str]): Nombres de las fabricas.
+        dias_semana (list[str]): Nombres de los dias de la semana.
+    """
+    print("Datos de Produccion:")
+    print(tabulate.tabulate(matriz, headers=dias_semana, showindex=nombres_fabricas, tablefmt="grid"))
     print()
 
-def matriz_a(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz a.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz a.
-    """
-    matriz = crear_matriz(n)
-    for i in range(n):
-        matriz[i][i] = 2 * i + 1
-    return matriz
 
-def matriz_b(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz b.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz b.
-    """
-    matriz = crear_matriz(n)
-    for i in range(n):
-        matriz[i][n - 1 - i] = 3 ** i
-    return matriz
+# Parametros
+num_fabricas = 4
+num_dias = 6
+max_produccion = 150
+dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado"]
+nombres_fabricas = [f"Fabrica {i+1}" for i in range(num_fabricas)]
 
-def matriz_c(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz c.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz c.
-    """
-    matriz = crear_matriz(n)    
-    for i in range(n):
-        for j in range(i + 1):
-            matriz[i][j] = n - i
-    return matriz
+# Crear matriz de produccion
+matriz_produccion = crear_matriz_azar(num_fabricas, num_dias, max_produccion)
+imprimir_datos(matriz_produccion, nombres_fabricas, dias_semana)
 
-def matriz_d(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz d.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz d.
-    """
-    matriz = crear_matriz(n)
-    
-    val = n ** 2
-    for i in range(n):
-        for j in range(n):
-            matriz[i][j] = int(val / 2)
-        val = val / 2
-    return matriz
+# Cantidad total por fabrica
+totales = total_por_fabrica(matriz_produccion)
+for i, total in enumerate(totales):
+    print(f"{nombres_fabricas[i]} produjo un total de {total} bicicletas.")
+print()
 
-def matriz_e(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz e.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz e.
-    """
-    matriz = crear_matriz(n)
-    contador = 1
+# Fabrica que mas produjo en un solo dia
+fabrica_max, dia_max, cantidad_max = fabrica_mas_produccion_dia(matriz_produccion)
+print(f"La {nombres_fabricas[fabrica_max]} fue la que mas produjo en un solo dia: {cantidad_max} bicicletas el dia {dias_semana[dia_max]}.")
+print()
 
-    for i in range(n):
-        for j in range(n):
-            if (i + j) % 2 == 1:
-                matriz[i][j] = contador
-                contador += 1
-    return matriz
-
-def matriz_f(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz f.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz f.
-    """
-    matriz = crear_matriz(n)
-    for i in range( 1, n + 1 ):             
-        triangular_i = i * ( i + 1 ) // 2  #triangular de i 
-        for j in range( 1, n + 1 ): 
-            if i + j > n:
-                matriz[ i - 1 ][ j - 1 ] = triangular_i - ( i + j - ( n + 1 )) #triangular de i - (i + j - (n + 1))
-    return matriz
-
-
-def matriz_g(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz g.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz g.
-    """
-    matriz = crear_matriz(n)
-    
-    superior = 0
-    inferior = n - 1
-    izquierda = 0
-    derecha = n - 1
-    
-    num = 1
-    
-    while superior <= inferior and izquierda <= derecha:
-
-        # Rellenar la fila superior
-        for col in range(izquierda, derecha + 1):
-            matriz[superior][col] = num
-            num += 1
-        superior += 1
-        
-        # Rellenar la columna derecha
-        for fila in range(superior, inferior + 1):
-            matriz[fila][derecha] = num
-            num += 1
-        derecha -= 1
-        
-        # Rellenar la fila inferior
-        if superior <= inferior:
-            for col in range(derecha, izquierda - 1, -1):
-                matriz[inferior][col] = num
-                num += 1
-            inferior -= 1
-        
-        # Rellenar la columna izquierda
-        if izquierda <= derecha:
-            for fila in range(inferior, superior - 1, -1):
-                matriz[fila][izquierda] = num
-                num += 1
-            izquierda += 1
-    
-    return matriz
-
-def matriz_h(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz h.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz h.
-    """
-    matriz = crear_matriz(n)
-    num = 1
-    for k in range(n * 2 - 1):
-        for i in range(k + 1):
-            j = k - i
-            if i < n and j < n:
-                matriz[i][j] = num
-                num += 1
-    return matriz
-
-def matriz_i(n: int) -> list[list[int]]:
-    """
-    Crea una matriz de tamaño n x n con los valores de la matriz i.
-    args:
-        n (int): Tamaño de la matriz (n x n).
-    returns:
-        list: Matriz de tamaño n x n con los valores de la matriz i.
-    """
-    pass
-    
-
-def main():
-    n = int(input("Ingrese el tamaño de la matriz (N x N): "))
-
-    imprimir_matriz(matriz_a(n), 'A')
-    imprimir_matriz(matriz_b(n), 'B')
-    imprimir_matriz(matriz_c(n), 'C')
-    imprimir_matriz(matriz_d(n), 'D')
-    imprimir_matriz(matriz_e(n), 'E')
-    imprimir_matriz(matriz_f(n), 'F')
-    imprimir_matriz(matriz_g(n), 'G')
-    imprimir_matriz(matriz_h(n), 'H')
-    # imprimir_matriz(matriz_i(n), 'I')
-
-if __name__ == "__main__":
-    main()
+# Dia mas productivo
+dia_productivo, cantidad_productiva = dia_mas_productivo(matriz_produccion)
+print(f"El dia mas productivo fue el {dias_semana[dia_productivo]} con un total de {cantidad_productiva} bicicletas producidas.")
+for i in range(num_fabricas):
+    print(f"La menor cantidad producida por {nombres_fabricas[i]} es: {menor_cantidad_por_fabrica(matriz_produccion)[i]}")

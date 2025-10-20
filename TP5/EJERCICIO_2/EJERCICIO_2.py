d84501c1239aae1f6ff3dee56b9d0001fa93c82d
Matias Hoffer <141982076+mhoffer1@users.noreply.github.com>
2025-10-19 23:09:59 -0300

    consignas TP5

---
+++ TP5/EJERCICIO_2/EJERCICIO_2.py
@@ -0,0 +1,5 @@
+"""Realizar una función que reciba como parámetros dos cadenas de caracteres con
+teniendo números reales, sume ambos valores y devuelva el resultado como un 
+número real. Devolver -1 si alguna de las cadenas no contiene un número válido, 
+utilizando manejo de excepciones para detectar el error.
+"""

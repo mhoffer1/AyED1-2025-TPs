a5db4fc3aa03649ee7a1648c52b89581fd271575
mhoffer1 <matiashoffer@hotmail.com>
2025-10-22 20:33:38 +0000

    ejercico 1 tp 4

--- TP4/EJERCICIO_1/EJERCICIO_1.py
+++ TP4/EJERCICIO_1/EJERCICIO_1.py
@@ -4,4 +4,30 @@ utilizar cadenas auxiliares ni rebanadas. Escribir además un programa que permi
 verificar su funcionamiento.
 """
 
-# test
+def es_capicua(cadena: str) -> bool:
+    """
+    Determina si una cadena es capicua de forma recursiva.
+    - args: cadena (str): La cadena de caracteres a verificar.
+    - returns: bool: True si la cadena es capicua, False en caso contrario.
+    """
+    def chequear(i: int) -> bool:
+
+        largo = len(cadena)
+
+        if i >= largo // 2:
+            return True
+        if cadena[i] != cadena[- 1 - i]:
+            return False
+        return chequear(i + 1)
+
+    return chequear(0)
+
+def main():
+    cadena = input("Ingrese una cadena de caracteres: ")
+    if es_capicua(cadena):
+        print(f"La cadena '{cadena}' es capicúa.")
+    else:
+        print(f"La cadena '{cadena}' no es capicúa.")
+
+if __name__ == "__main__":
+    main()
